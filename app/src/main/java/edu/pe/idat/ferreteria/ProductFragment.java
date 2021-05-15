34985ed9561f5dde41697ae31966e0d37521ae53
Mod <natalyortiz.401@gmail.com>
2021-05-14 21:08:08 -0500

    Eliminando y creando nuevos fragments

---
+++ app/src/main/java/edu/pe/idat/ferreteria/ProductFragment.java
@@ -0,0 +1,36 @@
+package edu.pe.idat.ferreteria;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ProductFragment extends Fragment {
+
+
+
+    public ProductFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_product, container, false);
+    }
+}
