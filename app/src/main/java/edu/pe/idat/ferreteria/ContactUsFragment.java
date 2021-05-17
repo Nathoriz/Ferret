39d3fd3d92de54f61cb4cc3e3ce70654307dc675
key <dkeykoc.2603@gmail.com>
2021-05-16 23:02:02 -0500

    creacion del diseño y logica del fragmento contactUs

--- app/src/main/java/edu/pe/idat/ferreteria/ContactUsFragment.java
+++ app/src/main/java/edu/pe/idat/ferreteria/ContactUsFragment.java
@@ -1,37 +1,152 @@
 package edu.pe.idat.ferreteria;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
+import java.util.regex.Pattern;
+
+import edu.pe.idat.ferreteria.databinding.FragmentContactUsBinding;
+import edu.pe.idat.ferreteria.modelo.ModelContactUs;
 
 public class ContactUsFragment extends Fragment {
 
-
+    private FragmentContactUsBinding binding;
+    private ModelContactUs enviodata;
 
     public ContactUsFragment() {
-        // Required empty public constructor
+
     }
-
-
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+        binding = FragmentContactUsBinding.inflate(inflater,container,false);
+        binding.btnenviar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                if (Validacion()){
+                    mostrarAlerta("registro lleno",
+                            Envio().getSexo()+" "+Envio().getNombre()+"\n"+
+                                    "su mensaje "+Envio().getAsunto()+" pon el siguiente texto: \n "+
+                                    Envio().getDescripcion()+"se envio correctamente.\n"+"estaremos comunicandonos con usted mediante " +
+                                    Envio().getCorreo()+" o el siguente numero " +Envio().getTelefono()+"\n"+
+                                    "Gracias por su contactarnos.");
+                    limpiar();
+                }
+
+
+            }
+        });
+        return binding.getRoot();
+    }
+
+    public void mostrarAlerta(String titulo, String mensaje){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage(mensaje)
+                .setTitle(titulo);
+
+        AlertDialog alert =builder.create();
+        alert.show();
+    }
+
+    public ModelContactUs Envio() {
+        enviodata=new ModelContactUs();
+            String asunto,nombre,correo,sexo="",descripcion;
+            int telefono,empresa;
+
+                asunto = binding.etasunto.getText().toString();
+                nombre = binding.etnombre.getText().toString();
+                correo = binding.etcorreo.getText().toString();
+                if (binding.rdbsr.isChecked()==true) {
+                    sexo=binding.rdbsr.getText().toString();
+
+                }if(binding.rdbsra.isChecked()==true){
+                    sexo=binding.rdbsra.getText().toString();
+                }
+                descripcion = binding.edtmensaje.getText().toString();
+                telefono=Integer.parseInt(binding.ettelefono.getText().toString());
+                if(binding.cbempresa.isChecked()){
+                    empresa=1;
+                }else {
+                    empresa = 0;
+                }
+                enviodata.setAsunto(asunto);
+                enviodata.setNombre(nombre);
+                enviodata.setCorreo(correo);
+                enviodata.setSexo(sexo);
+                enviodata.setDescripcion(descripcion);
+                enviodata.setTelefono(telefono);
+                enviodata.setEmpresa(empresa);
+
+
+        return enviodata;
+
+    }
+       public void limpiar(){
+        binding.etasunto.setText("");
+        binding.etnombre.setText("");
+           binding.etcorreo.setText("");
+           binding.edtmensaje.setText("");
+           binding.ettelefono.setText("");
+           binding.rdbsr.setSelected(false);
+           binding.rdbsra.setSelected(false);
+           binding.cbempresa.setSelected(false);
+
+
+       }
+    public boolean Validacion(){
+        String msj = "";
+        boolean respuesta = true;
+        if (binding.etasunto.getText().toString().length() == 0) {
+            msj = "Ingrese el asunto";
+            respuesta = false;
+            mostrarAlerta("Asunto", msj);
+
+        } else if (binding.etnombre.getText().toString().length() == 0) {
+            msj = "Ingrese su nombre";
+            respuesta = false;
+            mostrarAlerta("Nombre", msj);
+        } else if (binding.ettelefono.getText().toString().length() == 0) {
+            msj = "Ingrese su telefono";
+            respuesta = false;
+            mostrarAlerta("Telefono", msj);
+        } else if (binding.etcorreo.getText().toString().length() == 0) {
+            msj = "Ingrese su correo";
+            respuesta = false;
+            mostrarAlerta("Correo", msj);
+        } else if (binding.etcorreo.getText().toString().length() > 0) {
+            Pattern pattern = Patterns.EMAIL_ADDRESS;
+            if (pattern.matcher(binding.etcorreo.getText().toString()).matches()) {
+                respuesta = true;
+            } else {
+                msj = "Su correo " + binding.etcorreo.getText().toString() + " no es valido";
+                respuesta = false;
+                mostrarAlerta("correo", msj);
+            }
+        } else if (!binding.rdbsr.isChecked() && !binding.rdbsra.isChecked()) {
+            msj = "Elige una opcion Sr o Sra";
+            respuesta = false;
+            mostrarAlerta("Elige", msj);
+        } else if (binding.edtmensaje.getText().toString().length() == 0) {
+            msj = "Ingrese su mensaje";
+            respuesta = false;
+            mostrarAlerta("mensaje", msj);
+        } else {
+           respuesta = true;
+        }
+
+        return respuesta;
+
+    }
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        binding = null;
     }
 }
---
+++ app/src/main/java/edu/pe/idat/ferreteria/modelo/ModelContactUs.java
@@ -0,0 +1,129 @@
+package edu.pe.idat.ferreteria.modelo;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ModelContactUs implements Parcelable {
+
+    private String asunto;
+    private String nombre;
+    private int telefono;
+    private String correo;
+    private String sexo;
+    private String descripcion;
+    private int empresa;
+
+    public ModelContactUs() {
+
+    }
+
+    public ModelContactUs(String asunto, String nombre, int telefono, String correo, String sexo, String descripcion, int empresa) {
+
+        this.asunto = asunto;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.sexo = sexo;
+        this.descripcion = descripcion;
+        this.empresa = empresa;
+    }
+
+    protected ModelContactUs(Parcel in) {
+
+        asunto = in.readString();
+        nombre = in.readString();
+        telefono = in.readInt();
+        correo = in.readString();
+        sexo = in.readString();
+        descripcion = in.readString();
+        empresa = in.readInt();
+    }
+
+    public static final Creator<ModelContactUs> CREATOR = new Creator<ModelContactUs>() {
+        @Override
+        public ModelContactUs createFromParcel(Parcel in) {
+            return new ModelContactUs(in);
+        }
+
+        @Override
+        public ModelContactUs[] newArray(int size) {
+            return new ModelContactUs[size];
+        }
+    };
+
+
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(int empresa) {
+        this.empresa = empresa;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+        parcel.writeString(asunto);
+        parcel.writeString(nombre);
+        parcel.writeInt(telefono);
+        parcel.writeString(correo);
+        parcel.writeString(sexo);
+        parcel.writeString(descripcion);
+        parcel.writeInt(empresa);
+    }
+}
+
